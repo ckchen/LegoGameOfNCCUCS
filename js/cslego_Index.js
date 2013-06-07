77778af6443b8f0da9cdc89312a4336a1644158b
CheeryDachshund_Bryan <ibaseballbaby@gmail.com>
2013-06-07 08:50:33 +0800

    check it's modified

--- js/cslego_Index.js
+++ js/cslego_Index.js
@@ -110,7 +110,7 @@ var addThePicsResultsToPage = function(legosRetrieved){
 	//跑過底下所有legos的，指定每個lego的格子裡該用什麼屬性
 	loopTablesAndAssignCSS(legosRetrieved.length, legosRetrieved);
 
-} 
+}  
 
 var createTheArrayDisplay = function(lengthOfDisplayedLegos){
 
